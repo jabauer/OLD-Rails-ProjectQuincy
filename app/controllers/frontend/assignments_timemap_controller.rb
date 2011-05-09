55fe1b92e49d81ac7ea8969d0ef857364883ffd7
Jean Bauer <jab9fn@virginia.edu>
2011-05-09 17:28:20 -0400

    This commit impliments a very basic version of the assignments_timemap with data from John Quincy Adams' diplomatic career.  It simply displays all the locations as purple dots.
    It also corrects a typo that misrendered birth and death dates for an individuals show page.

--- app/controllers/frontend/assignments_timemap_controller.rb
+++ app/controllers/frontend/assignments_timemap_controller.rb
@@ -5,8 +5,10 @@ class Frontend::AssignmentsTimemapController < ApplicationController
   end
   
   def assign_byyear_data
-    @assignments = Assignment.find(:all)
+    @assignments = Assignment.find_by_location_id(:order => 'start_year ASC')
+#    @assignments = Assignment.find(:all)
     @assignment_titles = AssignmentTitle.find(:all)
+    @locations = Location.find(:all)
     
     respond_to do |format|
       format.js  { render :action => 'assign_byyear_data' }
